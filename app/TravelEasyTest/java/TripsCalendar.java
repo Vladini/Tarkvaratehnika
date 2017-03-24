bbe36f815369ae06ef467bc48bcf67170127d73f
ulguss <u.gussarova@gmail.com>
2017-03-24 21:20:11 +0200

    TripsCalendar

---
+++ app/TravelEasyTest/java/TripsCalendar.java
@@ -0,0 +1,13 @@
+package com.example.user.traveleasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripsCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips_calendar);
+    }
+}
